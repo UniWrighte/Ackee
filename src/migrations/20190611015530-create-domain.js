7f5a2019cf6342c97454d925efa835846278bf79
David Kamer <david.kamer@uniwrighte.com>
2019-06-11 00:09:41 -0400

    domain model

---
+++ src/migrations/20190611015530-create-domain.js
@@ -0,0 +1,28 @@
+'use strict'
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Domains', {
+      // TODO - make the domain ID uuidv3 on domain name.
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Domains')
+  }
+}
---
+++ src/models/domain.js
@@ -0,0 +1,17 @@
+'use strict'
+module.exports = (sequelize, DataTypes) => {
+  const Domain = sequelize.define('Domain', {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
+    title: DataTypes.STRING
+  }, {})
+  Domain.associate = function (models) {
+    // associations can be defined here
+    Domain.hasMany(models.Record)
+  }
+  return Domain
+}
