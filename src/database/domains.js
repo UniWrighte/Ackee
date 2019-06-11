0166484332a373f19022bfef10bdade44356ccc5
David Kamer <david.kamer@uniwrighte.com>
2019-06-11 00:10:35 -0400

    domain sequelize route adaptation

--- src/database/domains.js
+++ src/database/domains.js
@@ -1,51 +1,68 @@
 'use strict'
 
-const Domain = require('../schemas/Domain')
+// const Domain = require('../schemas/Domain')
+const db = require('../models')
 
+// const add = async (data) => {
+//   return Domain.create(data)
+// }
 const add = async (data) => {
-  return Domain.create(data)
+  return db.Domain.create(data)
 }
 
+// const all = async () => {
+//   return Domain.aggregate([
+//     {
+//       $addFields: {
+//         insensitive: {
+//           $toLower: '$title'
+//         }
+//       }
+//     },
+//     {
+//       $sort: {
+//         insensitive: 1
+//       }
+//     }
+//   ])
+// }
 const all = async () => {
-  return Domain.aggregate([
-    {
-      $addFields: {
-        insensitive: {
-          $toLower: '$title'
-        }
-      }
-    },
-    {
-      $sort: {
-        insensitive: 1
-      }
-    }
-  ])
+  return db.Domain.findAll()
 }
 
-const get = async (id) => {
-  return Domain.findOne({
-    id
-  })
+// const get = async (id) => {
+//   return Domain.findOne({
+//     id
+//   })
+// }
+const get = async id => {
+  return db.Domain.findOne({ where: { id } })
 }
+
+// const update = async (id, { title }) => {
+//   return Domain.findOneAndUpdate({
+//     id
+//   }, {
+//     $set: {
+//       title,
+//       updated: Date.now()
+//     }
+//   }, {
+//     new: true
+//   })
+// }
 
 const update = async (id, { title }) => {
-  return Domain.findOneAndUpdate({
-    id
-  }, {
-    $set: {
-      title,
-      updated: Date.now()
-    }
-  }, {
-    new: true
-  })
+  return db.Domain.update({ title }, { where: { id } })
 }
 
-const del = async (id) => {
-  return Domain.findOneAndDelete({
-    id
-  })
+// const del = async (id) => {
+//   return Domain.findOneAndDelete({
+//     id
+//   })
+// }
+const del = async id => {
+  return db.Domain.destroy({ where: { id } })
 }
 
 module.exports = {
