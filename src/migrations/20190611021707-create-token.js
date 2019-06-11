e41541f691a427eb9dd9d1e63f88397b2b4945af
David Kamer <david.kamer@uniwrighte.com>
2019-06-11 00:10:16 -0400

    token model

---
+++ src/migrations/20190611021707-create-token.js
@@ -0,0 +1,28 @@
+'use strict'
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Tokens', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      valid: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Tokens')
+  }
+}
